83d29884f7a9dca47e63afcdd451ce36a7d45f5e
lingPoint <44696270+lingPoint@users.noreply.github.com>
2021-06-14 13:40:15 +0800

    抓包获取数据

--- dxx.py
+++ dxx.py
@@ -8,7 +8,7 @@ import sys
 def main():
     url = "http://dxx.scyol.com/api/student/commit"
     agent = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; MI 8 SE Build/QKQ1.190828.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045617 Mobile Safari/537.36 MMWEBID/3048 MicroMessenger/8.0.3.1880(0x2800033F) Process/tools WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
+        "User-Agent": "抓包获取", #安全第一，用自己的请求头
         'Content-Type': 'application/json',
         "token":"抓包获取", #抓包获取！！！
     }
